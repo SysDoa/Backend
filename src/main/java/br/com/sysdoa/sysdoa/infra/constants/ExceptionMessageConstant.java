8b748a5e3a5072eeac4f72012476452e94fda12b
lazaroalexandre <lazaroalexandre2022@gmail.com>
2025-06-19 12:32:04 -0300

    feat: adicionando a camada de infra

---
+++ src/main/java/br/com/sysdoa/sysdoa/infra/constants/ExceptionMessageConstant.java
@@ -0,0 +1,15 @@
+package br.com.sysdoa.sysdoa.infra.constants;
+
+public final class ExceptionMessageConstant {
+
+    public static final String fileError = "Erro ao carregar o arquivo!";
+
+    public static final String attributeUsed(String attribute) {
+        return attribute + " já em uso!";
+    }
+
+    public static final String notFound(String name) {
+        return name + " não encontrado!";
+    }
+
+}
