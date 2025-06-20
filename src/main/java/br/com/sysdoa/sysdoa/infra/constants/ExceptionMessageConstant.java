0dde5cf6340abea33214eb946d4535080970c3ed
lazaroalexandre <lazaroalexandre2022@gmail.com>
2025-06-19 22:32:28 -0300

    fix: corrigindo as mensagens

--- src/main/java/br/com/sysdoa/sysdoa/infra/constants/ExceptionMessageConstant.java
+++ src/main/java/br/com/sysdoa/sysdoa/infra/constants/ExceptionMessageConstant.java
@@ -4,7 +4,7 @@ public final class ExceptionMessageConstant {
 
     public static final String errorDataBase = "Dados duplicados ou violação de integridade: ";
 
-    public static final String errorSave = "Erro ao salvar dados! ";
+    public static final String errorSave = "Erro ao salvar dados: ";
 
     public static final String notFound(String name) {
         return name + " não encontrado!";
